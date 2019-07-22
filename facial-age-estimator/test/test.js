47fd1d15b70d536e32325ae1afd1d99b4bedde4c
Daniel Jalova <djalova@us.ibm.com>
2019-07-22 15:42:30 -0700

    Fix style

--- facial-age-estimator/test/test.js
+++ facial-age-estimator/test/test.js
@@ -1,6 +1,6 @@
-const jimp = require('jimp');
-const nodeCanvas = require('canvas');
-const ageEstimator = require('../dist/max.ageest.cjs.js');
+const jimp = require('jimp')
+const nodeCanvas = require('canvas')
+const ageEstimator = require('../dist/max.ageest.cjs.js')
 
 const imagePath = `${__dirname}/face.jpg`
 
@@ -14,29 +14,27 @@ const createCanvasElement = function (imageInput) {
   })
 }
 
-describe("A suite", function() {
-
+describe('A suite', function () {
   const input = jimp.read(imagePath)
     .then(imageData => imageData.getBufferAsync(jimp.MIME_PNG))
     .then(imageBuffer => createCanvasElement(imageBuffer))
 
-
-  it("processInput, runInference, and processOutput", function() {
+  it('processInput, runInference, and processOutput', function () {
     return input.then(imageElement => ageEstimator.processInput(imageElement))
       .then(input => ageEstimator.runInference(input))
       .then(output => ageEstimator.processOutput(output))
-      .then(result => expect(result[0]).toBeCloseTo(36, 0));
-  });
+      .then(result => expect(result[0]).toBeCloseTo(36, 0))
+  })
 
-  it("predict function works", function() {
+  it('predict function works', function () {
     return input.then(imageElement => ageEstimator.predict(imageElement))
-      .then(result => expect(result[0]).toBeCloseTo(36, 0));
+      .then(result => expect(result[0]).toBeCloseTo(36, 0))
   })
 
-  it("multiple array works", function() {
+  it('multiple array works', function () {
     return input.then(imageElement => ageEstimator.processInput([imageElement, imageElement]))
-    .then(input => ageEstimator.runInference(input))
-    .then(output => ageEstimator.processOutput(output))
-    .then(result => expect(result.length).toEqual(2));
+      .then(input => ageEstimator.runInference(input))
+      .then(output => ageEstimator.processOutput(output))
+      .then(result => expect(result.length).toEqual(2))
   })
-});
+})
