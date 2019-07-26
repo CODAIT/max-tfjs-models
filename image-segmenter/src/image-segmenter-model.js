ffcecc7176d7b9eae6cd8d9606f80e05676f5784
va barbosa <va@us.ibm.com>
2019-07-26 09:40:10 -0400

    fix(cos): update model assets location

--- image-segmenter/src/image-segmenter-model.js
+++ image-segmenter/src/image-segmenter-model.js
@@ -5,7 +5,7 @@ let modelPath = null
 if (!process.rollupBrowser) {
   modelPath = `file://${__dirname}/../model/model.json`
 } else {
-  modelPath = 'https://max-assets.s3.us.cloud-object-storage.appdomain.cloud/image-segmenter/tf_js/0.1/model.json'
+  modelPath = 'https://s3.us-south.cloud-object-storage.appdomain.cloud/max-assets-prod/max-image-segmenter/tfjs/0.1.0/model.json'
 }
 let model = null
 let warmed = false
